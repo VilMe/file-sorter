ba473d97eef751f628439eebb0f664a6189d6c04
VilMe <vilma.medrano00@gmail.com>
2025-10-13 23:08:39 -0500

    define create_folder function to create folders for file types found

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 import os
 import shutil
 
+def create_folder(path: str, extension: str) -> str:
+    folder_name: str = extension[1:]
+    folder_path: str = os.path.join(path, folder_name)
+
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+
+    return folder_path
+
