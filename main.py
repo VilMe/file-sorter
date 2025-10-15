f0b5a5800f565ea1e88d305495f5b2e67662024d
VilMe <vilma.medrano00@gmail.com>
2025-10-14 21:48:49 -0500

    define sort_files function that takes a file path check the extensions, makes folders based on extension, and sorts into the folders

--- main.py
+++ main.py
@@ -13,3 +13,17 @@ def create_folder(path: str, extension: str) -> str:
 
     return folder_path
 
+
+def sort_files(source_path: str):
+    """Sorts files based on a given path"""
+
+    for root_dir, sub_dir, filenames in os.walk(source_path):
+        for filename in filenames:
+            file_path: str = os.path.join(root_dir, filename)
+            extension: str = os.path.splitext(filename)[1]
+
+            if extension:
+                target_folder: str = create_folder(source_path)
+                target_path: str = os.path.join(target_folder, filename)
+
+                shutil.move(file_path, target_path)
