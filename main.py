92c96a5c56f29c92eb5e26d74703de0ee75b9612
VilMe <vilma.medrano00@gmail.com>
2025-11-05 20:28:26 -0600

    resolve bug, update create_folder function in sort files function with missing variable

--- main.py
+++ main.py
@@ -18,20 +18,14 @@ def create_folder(path: str, extension: str) -> str:
 
 def sort_files(source_path: str):
     """Sorts files based on a given path"""
-    print('x'*15)
-    print(f'this is source_path {source_path}')
-    print('x'*15)
+    
     for root_dir, sub_dir, filenames in os.walk(source_path):
         for filename in filenames:
             file_path: str = os.path.join(root_dir, filename)
             extension: str = os.path.splitext(filename)[1]
-            print('x'*15)
-            print('this is the extension')
-            print(extension)
-            print('x'*15)
 
             if extension:
-                target_folder: str = create_folder(source_path)
+                target_folder: str = create_folder(source_path,extension=extension)
                 target_path: str = os.path.join(target_folder, filename)
 
                 shutil.move(file_path, target_path)
